749ec8d2d3869c71c954f408b9305a6e74574ccb
Cédric Belin <cedric@belin.io>
2019-10-07 20:14:38 +0200

    Added the CLI usage

---
+++ lib/Cli/usage.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+/** @var string The usage information. */
+return $usage = <<<'EOT'
+Open whatever you want, such as URLs, files or executables, regardless of the platform you use.
+
+Usage: open_cli [options]
+
+Options:
+-h, --help       Output usage information.
+-v, --version    Output the version number.
+EOT;
