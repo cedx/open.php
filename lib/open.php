44da472ef195c2eac9c29c08baf9fa0ef92fe86f
Cédric Belin <cedric@belin.io>
2019-10-10 17:16:47 +0200

    Fixed the behavior on Windows

--- lib/open.php
+++ lib/open.php
@@ -32,11 +32,11 @@ function open(string $target, array $options = []): Process {
   }
   else if ($isWindows) {
     $command[] = 'cmd.exe';
-    array_push($command, '/c', 'start', '', '/b');
+    array_push($command, '/c', 'start', '""', '/b');
     if ($wait) $command[] = '/wait';
-    if ($application) $command[] = $isWsl && mb_substr($application, 0, 5) == '/mnt/' ? Wsl::resolvePath($application) : $application;
-    if ($arguments) array_push($command, ...$arguments);
-    $command[] = str_replace('&', '^&', $target);
+    if ($application) $command[] = escapeshellarg($isWsl && mb_substr($application, 0, 5) == '/mnt/' ? Wsl::resolvePath($application) : $application);
+    if ($arguments) array_push($command, ...array_map('escapeshellarg', $arguments));
+    $command[] = str_replace('&', '^&', escapeshellarg($target));
   }
   else {
     $command[] = $application ?: 'xdg-open';
@@ -45,14 +45,11 @@ function open(string $target, array $options = []): Process {
     $command[] = $target;
   }
 
-  $process = $isWindows ? Process::fromShellCommandline('TODO' /* TODO */) : new Process($command);
+  $process = $isWindows ? Process::fromShellCommandline(implode(' ', $command)) : new Process($command);
   if ($outputDisabled) $process->disableOutput();
   $process->setTimeout(null);
-  $process->start();
-  if ($wait) {
-    $exitCode = $process->wait();
-    if ($exitCode > 0) throw new ProcessFailedException($process);
-  }
 
+  if ($wait) $process->mustRun();
+  else $process->run();
   return $process;
 }
