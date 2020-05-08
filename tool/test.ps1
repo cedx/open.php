781b7223417e8aa9cc8f3c358917c7d4f56df74f
Cédric Belin <cedric@belin.io>
2020-05-08 14:43:31 +0200

    Replaced the Robo system build by PowerShell scripts

---
+++ tool/test.ps1
@@ -0,0 +1,6 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+Set-Location (Split-Path $PSScriptRoot)
+
+Write-Host 'The test assertions must be verified manually as there is no way to check that they actually opened anything.'
+vendor/bin/phpunit --configuration=etc/phpunit.xml
