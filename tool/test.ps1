ad093f09c1bc911979bf7ada17866480065da20f
Cédric Belin <cedric@belin.io>
2020-06-05 18:40:12 +0200

    Updated the build system

--- tool/test.ps1
+++ tool/test.ps1
@@ -3,4 +3,4 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 Write-Host 'The test assertions must be verified manually as there is no way to check that they actually opened anything.'
-vendor/bin/phpunit --configuration=etc/phpunit.xml
+composer run-script test
