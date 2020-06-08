22da9fd1697f3b71256ef03611182b2e9b3f4e52
Cédric Belin <cedric@belin.io>
2020-06-08 15:59:13 +0200

    Updated the build system

--- tool/lint.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-Set-Location (Split-Path $PSScriptRoot)
-
-php -l bin/open_cli
-php -l example/main.php
-composer run-script lint
--- tool/test.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-Set-Location (Split-Path $PSScriptRoot)
-
-Write-Host 'The test assertions must be verified manually as there is no way to check that they actually opened anything.'
-composer run-script test
